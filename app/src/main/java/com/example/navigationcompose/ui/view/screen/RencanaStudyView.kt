f3b4666c8656302e4b57fcea485f6aa69434191c
arifrahmadi23 <muhammad231arif@gmail.com>
2024-11-21 20:45:29 +0700

    Membuat variabel listData di RencanaStudyView.kt

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
@@ -16,4 +16,5 @@ fun RencanaStudyView(
     var chosenDropdown by remember { mutableStateOf("") }
     var checked by remember { mutableStateOf(false) }
     var pilihanKelas by remember { mutableStateOf("") }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
 }
