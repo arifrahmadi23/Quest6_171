1610f41b373ad33ce939c5cbaab6fca84f01c657
arifrahmadi23 <muhammad231arif@gmail.com>
2024-11-21 21:42:32 +0700

    Membuat variabel private _krsState dan variabel krsStateUi

--- app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,6 +1,12 @@
 package com.example.navigationcompose.ui.view.viewmodel
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class RencanaStudyViewModel : ViewModel(){
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+
 }
