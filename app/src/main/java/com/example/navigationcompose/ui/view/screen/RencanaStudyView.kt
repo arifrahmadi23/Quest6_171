cf067341cf5487a2cd5e5006010ff68e1f1b05fc
arifrahmadi23 <muhammad231arif@gmail.com>
2024-11-21 20:47:06 +0700

    Membuat Column paling awal di RencanaStudyView.kt

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
@@ -1,10 +1,17 @@
 package com.example.navigationcompose.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.colorResource
+import com.example.navigationcompose.R
 import com.example.navigationcompose.model.Mahasiswa
 
 @Composable
@@ -17,4 +24,11 @@ fun RencanaStudyView(
     var checked by remember { mutableStateOf(false) }
     var pilihanKelas by remember { mutableStateOf("") }
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ){
+
+    }
 }
