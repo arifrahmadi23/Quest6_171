2196469b3c5a29c79ace98f208cb3bf349801395
arifrahmadi23 <muhammad231arif@gmail.com>
2024-11-21 20:41:59 +0700

    Membuat fungsi RencanaStudyView di RencanaStudyView.kt

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
@@ -1,2 +1,16 @@
 package com.example.navigationcompose.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.navigationcompose.model.Mahasiswa
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit ){
+
+}
