338b2f30395588d3d575788671e52237db765eff
arifrahmadi23 <muhammad231arif@gmail.com>
2024-11-21 20:19:25 +0700

    Membuat Fungsi DynamicSelectedField pada DynamicSelectedField.kt

--- app/src/main/java/com/example/navigationcompose/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/navigationcompose/ui/widget/DynamicSelectedField.kt
@@ -1,4 +1,16 @@
 package com.example.navigationcompose.ui.widget
 
+import android.media.metrics.Event
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
+@Composable
+fun DynamicSelectTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
