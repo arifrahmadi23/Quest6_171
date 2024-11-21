0d8546ee72bb070e21357edc783cbafaaf1585fa
arifrahmadi23 <muhammad231arif@gmail.com>
2024-11-21 20:53:05 +0700

    Menambahkan image pada row yang ada di dalam column di RencanaStudyView.kt

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
@@ -1,11 +1,14 @@
 package com.example.navigationcompose.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,8 +16,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.navigationcompose.R
 import com.example.navigationcompose.model.Mahasiswa
@@ -38,7 +43,13 @@ fun RencanaStudyView(
             .fillMaxWidth()
             .padding(16.dp), verticalAlignment = Alignment.CenterVertically
         ){
-            
+            Image(
+                painter = painterResource(id = R.drawable.fotoprofil),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
         }
     }
 }
