1e566c61fa02d958a398cefbfdc60d39477af79b
arifrahmadi23 <muhammad231arif@gmail.com>
2024-11-21 20:51:07 +0700

    Membuat Row di dalam Column paling awal di RencanaStudyView.kt

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
@@ -2,15 +2,20 @@ package com.example.navigationcompose.ui.view.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.example.navigationcompose.R
 import com.example.navigationcompose.model.Mahasiswa
 
@@ -29,6 +34,11 @@ fun RencanaStudyView(
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary))
     ){
-
+        Row (modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp), verticalAlignment = Alignment.CenterVertically
+        ){
+            
+        }
     }
 }
